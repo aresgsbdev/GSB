eab19d09662605c6836b1053526c5ec20406e3ea
unknown <boivinsamuel.98@gmail.com>
2021-03-25 10:37:29 +0100

    users fixtures 25032021

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,9 +19,27 @@ class UserFixtures extends Fixture
 	
     public function load(ObjectManager $manager)
     {
+
+		$superAdmin = new User();
+		
+		$superAdmin->setRoles(['ROLE_ADMIN', 'ROLE_SUPER_ADMIN', 'ROLE_COMPTABLE', 'ROLE_USER']);
+		$superAdmin->setEmail('superAdmin@superAdmin.fr');
+		$superAdmin->setLastName('superAdmin');
+		$superAdmin->setFirstName('superAdmin');
+		$superAdmin->setAddress('404ruedessuperAdmin');
+		$superAdmin->setCity('superAdmin');
+		$superAdmin->setPostalCode('42720');
+		$superAdmin->setHireDate(new DateTime());
+		$superAdmin->setRegistrationNumber('0123456789');
+		$superAdmin->setPhone('0123456789');
+		$superAdmin->setPassword($this->passwordEncoder->encodePassword(
+			$superAdmin,
+			'superAdmin'
+		));
+
 		$admin = new User();
 		
-		$admin->setRoles(['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']);
+		$admin->setRoles(['ROLE_ADMIN', 'ROLE_COMPTABLE', 'ROLE_USER']);
 		$admin->setEmail('admin@admin.fr');
 		$admin->setLastName('admin');
 		$admin->setFirstName('admin');
@@ -35,10 +53,47 @@ class UserFixtures extends Fixture
 			$admin,
 			'admin'
 		));
+
+		$comptable = new User();
+		
+		$comptable->setRoles(['ROLE_COMPTABLE']);
+		$comptable->setEmail('comptable@comptable.fr');
+		$comptable->setLastName('comptable');
+		$comptable->setFirstName('comptable');
+		$comptable->setAddress('404ruedescomptable');
+		$comptable->setCity('comptable');
+		$comptable->setPostalCode('42720');
+		$comptable->setHireDate(new DateTime());
+		$comptable->setRegistrationNumber('0123456789');
+		$comptable->setPhone('0123456789');
+		$comptable->setPassword($this->passwordEncoder->encodePassword(
+			$comptable,
+			'comptable'
+		));
+
+		$user = new User();
+		
+		$user->setEmail('user@user.fr');
+		$user->setLastName('user');
+		$user->setFirstName('user');
+		$user->setAddress('404ruedesuser');
+		$user->setCity('user');
+		$user->setPostalCode('42720');
+		$user->setHireDate(new DateTime());
+		$user->setRegistrationNumber('0123456789');
+		$user->setPhone('0123456789');
+		$user->setPassword($this->passwordEncoder->encodePassword(
+			$user,
+			'user'
+		));
+
         // $product = new Product();
         // $manager->persist($product);
 
+		$manager->persist($superAdmin);
 		$manager->persist($admin);
+		$manager->persist($comptable);
+		$manager->persist($user);
         $manager->flush();
     }
 }
